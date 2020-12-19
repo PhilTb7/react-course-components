e3cb8840e1e5b1d43c64af6053b6edfcb40a9579
Philipp Olovsson <philipp.olovsson@outlook.com>
2020-12-19 17:22:22 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName() + " " + faker.name.lastName()}
+                    timeAgo={faker.date.recent(7).toLocaleString()}
+                    image={faker.image.avatar()}
+                    message={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName() + " " + faker.name.lastName()}
+                    timeAgo={faker.date.recent(7).toLocaleString()}
+                    image={faker.image.avatar()}
+                    message={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName() + " " + faker.name.lastName()}
+                    timeAgo={faker.date.recent(7).toLocaleString()}
+                    image={faker.image.avatar()}
+                    message={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
